542c3327d0aad68aac8629fca46f963c2d5fc59a
Umer Khayam Mir <umerkhayam3643@gmail.com>
2023-03-09 16:19:27 +0530

    This is a C program which multiplies two number

---
+++ 2_number_multiplication.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float n1,n2;
+    printf("Enter number 1: ");
+    scanf("%f",&n1);
+    printf("Enter number 2: ");
+    scanf("%f",&n2);
+    printf("The multiplication of two number is %.2f",n1*n2);
+    return 0;
+}
