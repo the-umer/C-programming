9b7f3b87dfe12b59d072fbebd511bef7b9419fa1
Umer Khayam Mir <umerkhayam3643@gmail.com>
2023-03-10 16:02:37 +0530

    multiplication_of_n_numbers

---
+++ multiplication_of_n_number.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    float mul=1;
+    printf("Enter the total numbers to be multifplied:- ");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        float x;
+        printf("Enter %d Element: ",(i+1));
+        scanf("%f",&x);
+        mul=mul*x;
+    }
+    printf("multiplication of %d elements are: %.2f",n,mul);
+
+    return 0;
+}
